4480cd4c04b6c1419d95c996b6dd603eb42afbe6
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2023-03-05 15:17:08 +0100

    improve docs

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pycalaos",
-    version="0.0.1",
+    version="0.0.2",
     description="Calaos home automation client library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Sébastien Maccagnoni",
     author_email="sebastien@maccagnoni.eu",
     url="https://github.com/tiramiseb/pycalaos",
